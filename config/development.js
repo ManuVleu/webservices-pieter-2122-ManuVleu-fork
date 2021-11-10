b31f27bc4d1623e04d5ecdf2f4e6cf9fa01a7426
Manu Vleurick <manu.vleurick@student.hogent.be>
2021-11-10 19:11:21 +0100

    logger and index.js gemaakt

--- config/development.js
+++ config/development.js
@@ -3,4 +3,9 @@ module.exports = {
         level: 'silly',
         disabled: false,
     },
+
+    cors: {
+		origins: ['http://localhost:3000'],
+		maxAge: 3 * 60 * 60,
+	},
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
 const Koa = require('koa');
+const koaCors = require('@koa/cors');
+const Router = require('@koa/router');
 const config = require('config');
-const { getLogger } = require('./core/logging')
+const { initializeLogger,getLogger } = require('./core/logging');
+const bodyParser = require('koa-bodyparser');
+//add rest en data
 
+const NODE_ENV = config.get('env');
+const CORS_ORIGINS = config.get('cors.origins');
+const CORS_MAX_AGE = config.get('cors.maxAge');
 const LOG_LEVEL = config.get('log.level');
 const LOG_DISABLED = config.get('log.disabled');
 
-console.log(`log level ${LOG_LEVEL}, logs enabled: ${LOG_DISABLED !== true}`)
+async function main(){
+	initializeLogger({
+		level: LOG_LEVEL,
+		disabled: LOG_DISABLED,
+		isProduction: NODE_ENV === 'production',
+		defaultMeta: { NODE_ENV },
+	});
 
-const app = new Koa();
-const logger = getLogger();
+	//await initializeData();
 
+	const app = new Koa();	
+	
+	app.use(
+		koaCors({
+			origin: (ctx) => {
+				if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
+					return ctx.request.header.origin;
+				}
+				
+				return CORS_ORIGINS[0];
+			},
+			allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+			maxAge: CORS_MAX_AGE,
+		})
+	);
 
-app.use(async (ctx, next) => {
-	ctx.body = 'skgfhhhhhrttttttttttttttttdsss';
-	next();
-});
+  const logger = getLogger();
+	
+  app.use(bodyParser());
+	
+  //installRest(app);
+	
+  app.listen(9000);
+  logger.info(`🚀< Server listening on http://localhost:9000`);
+}
 
-logger.info(`🚀 Server listening on http://localhost:9000`);
-app.listen(9000);
+main();
