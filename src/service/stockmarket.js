edd5997937dc95b1d7abfd69a006ab298c473449
Manu Vleurick <manu.vleurick@student.hogent.be>
2021-11-22 11:12:13 +0100

    service laag nu echt volledig klaar

--- src/service/stockmarket.js
+++ src/service/stockmarket.js
@@ -1,53 +1,75 @@
+const config = require('config');
 const { getChildLogger } = require('../core/logging');
-//Ophalen data hier. const STOCKMARKETS
-//getGebruikersID
+const stockmarketRepo = require('../repository/stockmarket');
+const gebruikerService = require('../service/gebruiker');
 
 const debugLog = (message, meta = {}) => {
 	if (!this.logger) this.logger = getChildLogger('stockmarket-service');
 	this.logger.debug(message, meta);
 };
 
-const getAll = () => {
-	debugLog('Ophalen van alle STOCKMARKETS');
-	return { data: STOCKMARKETS, count: STOCKMARKETS.length };
-};
+/**
+ * Get the stockmarket with the given `gebruikersid`.
+ *
+ * @param {number} gebruikersid - id van de gebruiker van de stockmarket to get.
+ */
+const getById = async (gebruikersid) => {
+	debugLog(`Ophalen stockmarket met gebruikersid ${gebruikersid}`);
+	const stockmarket = stockmarketRepo.findById(gebruikersid);
 
-const getById = (id) => {
-	debugLog(`Ophalen stockmarket met id ${id}`);
-	return STOCKMARKETS.filter((stockmarket) => stockmarket.id === id)[0];
-};
+	if(!stockmarket){
+		throw new Error(`Er bestaat geen stockmarket voor de gebruiker met gebruikersid ${gebruikersid}`);
+	}
 
-const create = () => {
-    
-	const newStockmarket = { id: getGebruikerID(), bedrijfA: 0, bedrijfB: 0, bedrijfC: 0 };
-	debugLog('Nieuwe stockmarket gemaakt', newStockmarket);
-	//Voeg stockmarket toe aan STOCKMARKETS
-	return newStockmarket;
-};
-
-const updateById = (id, { bedrijfA, bedrijfB, bedrijfC }) => {
-	debugLog(`Updating stockmarket met id ${id}`, { bedrijfA, bedrijfB, bedrijfC });
-    //index van stockmarket in STOCKMARKETS
-	//const index = STOCKMARKETS.findIndex((stockmarket) => stockmarket.id === id);
-
-    /*
-	if (index < 0) return null;
-
-	const stockmarket = STOCKMARKETS[index];
-	stockmarket.naam = naam;
-	stockmarket.eindDatum = eindDatum;
-    stockmarket.punten = punten;
-    */
 	return stockmarket;
 };
 
+/**
+ * Create a new stockmarket.
+ *
+ * @param {object} stockmarket - The stockmarket to create.
+ * @param {number} stockmarket.gebruikersid - de id van de gebruiker voor zijn stockmarket.
+ */
+const create = async ({gebruikersnaam}) => {
+    
+	debugLog('Nieuwe stockmarket gemaakt', { gebruikersnaam });
+	
+	// For now simply create a new user every time
+	const { id: gebruikerID } = await gebruikerService.register({ naam: gebruikersnaam });
+
+	return stockmarketRepo.create({
+		gebruikerID,
+	});
+};
+
+/**
+ * Update an existing stockmarket.
+ *
+ * @param {string} id - Id van de gebruiker om zijn stockmarkets up te daten.
+ * @param {object} stockmarket - The stockmarket data to save.
+ * @param {string} [stockmarket.geldBedrijfA] - Geld geïnvesteerd in bedrijf A.
+ * @param {Date} [stockmarket.geldBedrijfB] - Geld geïnvesteerd in bedrijf B.
+ * @param {string} [stockmarket.geldBedrijfC] - Geld geïnvesteerd in bedrijf C.
+ */
+const updateById = (gebruikersID) => {
+	debugLog(`Updating stockmarket met gebruikersid ${gebruikersID}`, { gewoonteIDMeest, meestGeld, meestStockmarket,geld });
+    
+	return stockmarketRepo.updateById(id, {
+		geldBedrijfA,geldBedrijfB,geldBedrijfC,
+	});
+};
+
+/**
+ * Delete the stockmarket with the given `id`.
+ *
+ * @param {number} id - Id van de gebruiker om zijn stockmarket te deleten.
+ */
 const deleteById = (id) => {
 	debugLog(`Verwijderen van stockmarket met id ${id}`);
-	//STOCKMARKETS = STOCKMARKETS.filter((stockmarket) => stockmarket.id !== id);
+	await stockmarketRepo.deleteById(id);
 };
 
 module.exports = {
-	getAll,
 	getById,
 	create,
 	updateById,
