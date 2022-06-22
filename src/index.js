341460158f54ad952a10b0b1fffb7d6ca018ab87
Manu Vleurick <manu.vleurick@student.hogent.be>
2022-06-22 18:47:59 +0200

    removed package-lock.json

--- src/index.js
+++ src/index.js
@@ -18,5 +18,6 @@ async function main() {
   }
 }
 
-// Wrap inside a main function as top level await is not supported in all NodeJS versions
+
+// Wrap inside a main function as top level await is not supported in all NodeJS versions.
 main();
